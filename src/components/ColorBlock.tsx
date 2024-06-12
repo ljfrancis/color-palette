5d1db64b0d11caa893ecac75dcca029d75912706
Lydia Francis <lydiajanefrancis@gmail.com>
2024-06-12 16:57:52 -0500

    Additional responsive breaks for mobile

--- src/components/ColorBlock.tsx
+++ src/components/ColorBlock.tsx
@@ -6,13 +6,13 @@ export function ColorBlock({ hex, width, height }) {
 	const cleanHex = tinycolor(hex).toHex();
 	return (
 		<div 
-			className="w-full rounded-xl block flex shrink-0 items-center justify-center lg:m-2 m-1 lg:px-2 px-1"
+			className="w-full rounded-xl block flex shrink-0 items-center justify-center lg:m-2 md:m-1 m-0.5 lg:px-2 md:px-1 px-0.5"
 			style={{
 				backgroundColor: `#${cleanHex}`,
 				height: `calc(${height} * 2vw)`,
 				width: `${width}%`
 			}}>
-            <Link className={`text-sm md:text-lg lg:text-2xl ${isLight ? 'hex-text-black' : 'hex-text-white'}`} href={`/color?hex=${cleanHex}`}>#{cleanHex}</Link>
+            <Link className={`text-xs md:text-lg lg:text-2xl ${isLight ? 'hex-text-black' : 'hex-text-white'}`} href={`/color?hex=${cleanHex}`}>#{cleanHex}</Link>
         </div>
 	)
 }
